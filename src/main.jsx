e968fc5a96c7d10318867f57cc1b9ab69d641ca1
AsmSaikat <asmsaikat@gmail.com>
2024-09-22 08:04:31 +0600

    added navbar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root";
+import Home from "./components/Home/Home";
+import Jobs from "./components/Jobs";
+import AppliedJobs from "./components/AppliedJobs";
+import Blogs from "./components/Blogs";
+import Statistics from "./components/Statistics";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root></Root>,
+		children: [
+			{
+				path: "/",
+				element: <Home></Home>,
+			},
+			{
+				path: "/jobs",
+				element: <Jobs></Jobs>,
+			},
+			{
+				path: "/applied",
+				element: <AppliedJobs></AppliedJobs>,
+			},
+			{
+				path: "/blogs",
+				element: <Blogs></Blogs>,
+			},
+			{
+				path: "/statistics",
+				element: <Statistics></Statistics>,
+			},
+		],
+	},
+]);
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
